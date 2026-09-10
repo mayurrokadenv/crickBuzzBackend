d4cc51f7c146050bfd33778a7b17c205599b4ed6
Akash Yadav <akash.yadav@newvision-software.com>
2026-09-10 15:09:27 +0530

    Added Scorecard Data in Signalr Event

--- src/MatchApi.Domain/Common/ScoreUpdateDto.cs
+++ src/MatchApi.Domain/Common/ScoreUpdateDto.cs
@@ -1,15 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿
 
-namespace MatchApi.Domain.Common
-{
-    public record ScoreUpdateDto(
+namespace MatchApi.Domain.Common;
+
+public record ScoreUpdateDto(
     Guid FixtureId,
     int HomeRuns,
     int HomeWickets,
     string? HomeOvers,
     int AwayRuns,
     int AwayWickets,
-    string? AwayOvers);
-}
+    string? AwayOvers,
+    IReadOnlyList<ScorecardUpdateDto> Scorecards);
---
+++ src/MatchApi.Domain/Common/ScorecardupdateDTO.cs
@@ -0,0 +1,30 @@
+﻿public record ScorecardUpdateDto(
+    Guid Id,
+    Guid FixtureId,
+    int InningsNo,
+    Guid BattingTeamId,
+    Guid BowlingTeamId,
+    IReadOnlyList<BattingFigureUpdateDto> BattingFigures,
+    IReadOnlyList<BowlingFigureUpdateDto> BowlingFigures);
+
+public record BattingFigureUpdateDto(
+    Guid Id,
+    Guid PlayerId,
+    string PlayerName,
+    int Runs,
+    int Balls,
+    int Fours,
+    int Sixes,
+    decimal StrikeRate);
+
+public record BowlingFigureUpdateDto(
+    Guid Id,
+    Guid PlayerId,
+    string PlayerName,
+    string Overs,
+    int Maidens,
+    int Runs,
+    int Wickets,
+    int NoBalls,
+    int Wides,
+    decimal Economy);
